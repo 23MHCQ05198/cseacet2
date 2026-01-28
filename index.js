35db9db5e89f6ac90c7fb77b568086b847438b2a
Ramya Tamarana <147488087+23MHCQ05198@users.noreply.github.com>
2026-01-28 09:46:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/homepage",(req,res)=>{
+app.get("/",(req,res)=>{
     res.send("Welcome to awsacet2");
 });
 app.listen(3000,()=>{
